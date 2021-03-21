98eb543381b9c54064705b95a8c64ee5a0ff153e
CWhetha <44300934+CWhetha@users.noreply.github.com>
2021-03-21 18:16:51 -0400

    Revert "Player Movement/ Beach"
    This reverts commit 2403430930bfcfe8743c2530a6283722a17d0dfa.

---
+++ AtuaMotuGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+    Vector2 movement;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Vector2.zero;
+        // Input: Left arrow = -1, Right arrow = 1, None = 0
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        // Movement: updates at a fixed amount per unit time
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
