825f34ddb8c62f2e66fb47b839639ae4885298eb
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-03-21 07:22:46 -0400

    Movement animation changes, added IceCave scene

--- AtuaMotuGame/Assets/Scripts/PlayerMovement.cs
+++ AtuaMotuGame/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
 
     public Rigidbody2D rb;
     public Animator animator;
 
+    private Vector2 lastMovement;
+    private Vector2 movement;
 
-    Vector2 movement;
+    public bool isOnIce = false;
 
 
     void Start() {
@@ -21,19 +23,30 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         movement = Vector2.zero;
-        // Input: Left arrow = -1, Right arrow = 1, None = 0
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
+        if (!isOnIce) { 
+ 
+            //// Input: Left arrow = -1, Right arrow = 1, None = 0
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            if (movement.x != 0 || movement.y != 0)
+            {
+                lastMovement = movement;
+            }
+        }
+        animator.SetFloat("Horizontal", lastMovement.x);
+        animator.SetFloat("Vertical", lastMovement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
     }
 
     void FixedUpdate()
     {
         // Movement: updates at a fixed amount per unit time
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (isOnIce)
+        {
+            rb.MovePosition(rb.position + lastMovement * moveSpeed * Time.fixedDeltaTime);
+        } else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }
