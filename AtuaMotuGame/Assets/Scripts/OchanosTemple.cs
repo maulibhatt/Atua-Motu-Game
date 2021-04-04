067e345cd9df018161f5fe664b206c18f7740d62
mbhatt26 <mbhatt26@uwo.ca>
2021-04-04 04:38:01 -0400

    created final boss script, UI, enable trigger

---
+++ AtuaMotuGame/Assets/Scripts/OchanosTemple.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OchanosTemple : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool playerInRange;
+    public SignalSender clue;
+    public GameObject theBDC;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.Return))
+        {
+            // activate temple
+            Debug.Log("Temple Canvas activate!");
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInRange = true;
+            clue.Raise();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+            clue.Raise();
+        }
+    }
+
+
+}
