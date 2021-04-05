f1422c2dcd9727f5d34ebc7532d08b5743ce35e2
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-04-05 14:46:55 -0400

    a cubic crap load of stuff

--- AtuaMotuGame/Assets/BreakableTile.cs
+++ AtuaMotuGame/Assets/BreakableTile.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class BreakableTile : MonoBehaviour
 {
     public Tilemap breakableTilemap;
     public bool playerInRange;
     public GameObject player;
+
+    public GameObject digText;
+    public int currentDigs;
+    public int intialDigs = 15;
     // Start is called before the first frame update
     void Start()
     {
         //breakableTilemap = GetComponent<Tilemap>();
         playerInRange = false;
+        currentDigs = intialDigs;
+        updateDigs();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange && GameState.CheckActiveString("Erised") && currentDigs > 0)
         {
+            currentDigs--;
+            updateDigs();
             var tilepos = player.transform.position;
             breakableTilemap.SetTile(breakableTilemap.WorldToCell(tilepos), null);
             breakableTilemap.SetTile(breakableTilemap.WorldToCell(tilepos + Vector3.up), null);
@@ -45,6 +54,12 @@ public class BreakableTile : MonoBehaviour
             playerInRange = false;
         }
     }
+
+    void updateDigs()
+    {
+        digText.gameObject.GetComponent<Text>().text = ": " + currentDigs.ToString();
+    }
+
 
 
 
